95393eda2c7d60d0aa37be1f4977a9ccf202c302
⎝⎝ＶＯＸ ⎠⎠ <vox@ailoom.me>
2025-09-22 18:33:07 +0700

    update
    Co-authored-by: sumin1203 <sumin1203@users.noreply.github.com>

--- webapp/backend_K.py
+++ webapp/backend_K.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from supabase import create_client, Client
+from typing import Any, cast, Optional
 
-url: str = st.secrets["SUPABASE_URL"]
-key: str = st.secrets["SUPABASE_KEY"]
-supabase: Client = create_client(url, key)
+@st.cache_resource
+def init_connection() -> Client:
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase: Client = init_connection()
+_auth = cast(Any, supabase.auth)
+
